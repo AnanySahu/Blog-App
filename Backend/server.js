cf3974ba8a676647a8b94f94bf15049f54b04cba
AnanySahu <varunsahu237@gmail.com>
2026-06-06 16:07:54 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const authRoutes = require("./Routes/auth");
 const postRoutes = require("./Routes/posts");
 const app=express();
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("Uploads"));
 app.use("/api/auth", authRoutes);
 // app.use("/uploads", express.static("uploads"));
 mongoose.connect(process.env.MONGO_URI)
@@ -33,4 +34,8 @@ app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
