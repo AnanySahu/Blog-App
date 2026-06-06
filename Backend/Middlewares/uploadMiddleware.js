27611399a9242c5f0fb9fa76249c45e08d029335
AnanySahu <varunsahu237@gmail.com>
2026-06-06 17:53:28 +0530

    Image posting fixed

--- Backend/Middlewares/uploadMiddleware.js
+++ Backend/Middlewares/uploadMiddleware.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "Uploads/");
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + path.extname(file.originalname);
